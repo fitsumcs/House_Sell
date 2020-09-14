617a8bb849a08519451c0beb52e5ae92d5db4de9
Fitsum A <fitsum@gmail.com>
2020-09-14 13:10:45 +0300

    Starting express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi there..");
+const express = require('express');
+
+// port 
+const port = process.env.PORT || 3000;
+
+// The app 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi There");
+});
+
+app.listen(port, () => console.log("Server Started"));
