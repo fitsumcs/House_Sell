09daca6c2f1598a340b57b2a717b178492c42686
Fitsum A <fitsum@gmail.com>
2020-09-14 13:37:48 +0300

    Adding the index and houses page'

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const port = process.env.PORT || 3000;
 // The app 
 const app = express();
 
+// config 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-    res.send("Hi There");
+    res.render('index');
+});
+app.get('/homes', (req, res) => {
+    const homes = [
+        { title: "Two By TWo", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
+        { title: "Two By Three", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
+        { title: "Two By Four", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
+        { title: "Two By Five", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" }
+    ];
+    res.render('home', { homes });
 });
 
 app.listen(port, () => console.log("Server Started"));
