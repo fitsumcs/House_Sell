4a9ee179aaf06e89c210a6c232275d541a8ddeaf
Fitsum A <fitsum@gmail.com>
2020-09-14 14:33:23 +0300

    Adding post request for adding new house

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 // port 
 const port = process.env.PORT || 3000;
 
+// fake data 
+const homes = [
+    { title: "Two By TWo", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
+    { title: "Two By Three", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
+    { title: "Two By Four", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
+    { title: "Two By Five", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" }
+];
 // The app 
 const app = express();
 
 // config 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.render('index');
 });
-app.get('/homes', (req, res) => {
-    const homes = [
-        { title: "Two By TWo", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
-        { title: "Two By Three", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
-        { title: "Two By Four", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" },
-        { title: "Two By Five", image: "https://image.shutterstock.com/image-photo/young-man-sale-board-selling-260nw-730777636.jpg" }
-    ];
+app.get('/houses', (req, res) => {
     res.render('home', { homes });
 });
+// new house form view
+app.get('/houses/new', (req, res) => {
+    res.render('newHouse');
+});
+// add new house 
+app.post('/houses', (req, res) => {
 
+    //grab data from form 
+    const title = req.body.title;
+    const image = req.body.image;
+    const data = { title, image };
+    homes.push(data);
+    res.redirect('/houses');
+
+});
 app.listen(port, () => console.log("Server Started"));
