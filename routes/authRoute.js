dcfe31d5ec901b37cb423d30367d1617aa0a167c
3020 <fitsumcs@gmail.com>
2020-09-18 17:55:49 +0300

    Adding failureFlash

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -28,7 +28,8 @@ router.get('/login', (req, res) => {
 });
 router.post('/login', passport.authenticate('local', {
     successRedirect: '/houses',
-    failureRedirect: '/login'
+    failureRedirect: '/login',
+    failureFlash: true
 }), (req, res) => {});
 //logout
 router.get('/logout', (req, res) => {
