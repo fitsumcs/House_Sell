efdcff6d3187488028d862d93a8a8dab61e1a5e4
3020 <fitsumcs@gmail.com>
2020-10-02 13:20:10 +0300

    change to admin

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     password: String,
     role: {
         type: String,
-        default: "user"
+        default: "admin"
     }
 }, { timestamps: true });
 userSchema.plugin(passportLocalMongoose);
